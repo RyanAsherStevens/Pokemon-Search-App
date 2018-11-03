19e0fdbff0c556894260b72b829a9755f6cf8e15
Ryan Stevens <rastev03@gmail.com>
2018-11-03 14:57:45 -0400

    commented out

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Abilities from './Abilities';
+// import Abilities from './Abilities';
 
 // import {
 //     BrowserRouter,
