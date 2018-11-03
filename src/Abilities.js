de6fca462364c33338ddff8a87fb9e10851c33fe
Ryan Stevens <rastev03@gmail.com>
2018-11-03 15:33:32 -0400

    commenting more out

--- src/Abilities.js
+++ src/Abilities.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 
 // const abilityPromises = json.abilities.map(async (a) => {
 //     const data = await fetch(a.ability.url)
@@ -9,4 +9,3 @@ import React from 'react';
 //   const abilities = await Promise.all(abilityPromises)
 
 //   this.setState({abilities: abilities})
-
