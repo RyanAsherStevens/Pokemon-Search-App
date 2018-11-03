2d7aba188919f7ad022b5cdfbc685326084efb5c
Ryan Stevens <rastev03@gmail.com>
2018-11-03 14:22:27 -0400

    tried something with abilities

--- src/Abilities.js
+++ src/Abilities.js
@@ -1,13 +1,23 @@
 // import React from 'react';
 
-// const abilityPromises = json.abilities.map(async (a) => {
-//     const data = await fetch(a.ability.url)
-//     const json = await data.json() 
-//     return json
-//   })
+// const Ability = (props) => {
+//   const abilities = props.results.map(r => (
+//     <li key = {r.id}>
+//     </li>
+//   ))
+//   return <ul>{abilities}</ul>
+//   }
 
-//   const abilities = await Promise.all(abilityPromises)
+// export default Abilities 
 
-//   this.setState({abilities: abilities})
 
-// export default abilities
+
+// json.abilities.map(async (a) => {
+//   const data = await fetch(a.ability.url)
+//   const json = await data.json() 
+//   return json
+// })
+
+// const abilities = await Promise.all(abilityPromises)
+
+// this.setState({abilities: abilities})
