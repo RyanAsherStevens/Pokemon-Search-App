d79b34b694b0717a4b32b41c8d2dedd749dd7720
Ryan Stevens <rastev03@gmail.com>
2018-11-04 18:46:09 -0500

    imported more

--- src/App.js
+++ src/App.js
@@ -10,5 +10,18 @@ import Pokemon from './Pokemon';
 import PokemonList from './PokemonList';
 import SelectedPokemon from './SelectedPokemon';
 
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+
+      <Switch>
+        <Route exact path="/" component={Pokemon} />
+        <Route path="/pokemon" component={Pokemon} />
+        <Route path="/selectedpokemon" component={SelectedPokemon} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import Abilities from './Abilities';
 
-// import {
-//     BrowserRouter,
-//     Route,
-//     Switch
-//   } from 'react-router-dom';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
