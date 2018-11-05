cfc6bcdcaca237862121b21d09e477e1a8c33d66
Ryan Stevens <rastev03@gmail.com>
2018-11-04 19:26:29 -0500

    commented out

--- src/SelectedPokemon.js
+++ src/SelectedPokemon.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Pokemon from './Pokemon';
-import PokemonList from './PokemonList';
+// import React from 'react';
+// import Pokemon from './Pokemon';
+// import PokemonList from './PokemonList';
 
-<Route exact path="/" component={PokemonList} />
-<Route path="/pokemon" component={PokemonList} />
-<Route path="/pokemon/:id"
-              render={props => (
-                  <Pokemon key={props.match.params.id} {...props} />
-               )}
-/>
+// <Route exact path="/" component={PokemonList} />
+// <Route path="/pokemon" component={PokemonList} />
+// <Route path="/pokemon/:id"
+//               render={props => (
+//                   <Pokemon key={props.match.params.id} {...props} />
+//                )}
+// />
 
-export default SelectedPokemon;
+// export default SelectedPokemon;
