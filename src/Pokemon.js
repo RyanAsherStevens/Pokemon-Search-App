2a07e61c70b53f565d33163d7ab5585653931c60
Ryan Stevens <rastev03@gmail.com>
2019-12-08 21:20:56 -0500

    reverted back to original api fetch url

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -15,7 +15,7 @@ class Pokemon extends Component {
 async componentDidMount() {
     const res =
         await fetch(
-            `https://pokeapi.co/api/v2/pokemon/?offset=1&limit=964${this.props.match.params.name}/`,
+            `https://pokeapi.co/api/v2/pokemon/${this.props.match.params.name}/`,
             { cache: "force-cache" })
             const json = await res.json()
   
